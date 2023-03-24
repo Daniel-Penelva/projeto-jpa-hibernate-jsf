30acd6b2f7188392cb3fc4e35086b2836b200923
Daniel Penelva <d4n.andrade@gmail.com>
2023-03-23 21:27:36 -0300

    Criando projeto e entidade e config. persistence/faces-config e web.xml

---
+++ projeto-jpa-hibernate-jsf/src/test/java/br/com/projetoJpaHibernateJsf/entidade/teste/TesteJPA.java
@@ -0,0 +1,13 @@
+package br.com.projetoJpaHibernateJsf.entidade.teste;
+
+import javax.persistence.Persistence;
+
+public class TesteJPA {
+
+	public static void main(String[] args) {
+		
+		Persistence.createEntityManagerFactory("projeto-jpa-hibernate-jsf");
+
+	}
+
+}
