9992a91b4fc3cf8737cab20139ce1a12566c5c03
Daniel Penelva <d4n.andrade@gmail.com>
2023-03-31 21:31:54 -0300

    Transformando o cadastro de pessoa em usuário do sistema

--- projeto-jpa-hibernate-jsf/src/main/java/br/com/projetoJpaHibernateJsf/entidade/Pessoa.java
+++ projeto-jpa-hibernate-jsf/src/main/java/br/com/projetoJpaHibernateJsf/entidade/Pessoa.java
@@ -31,6 +31,10 @@ public class Pessoa implements Serializable {
 	private String sexo;
 	private String[] frameworks;
 	private Boolean ativo;
+	
+	private String login;
+	private String senha;
+	private String perfilUser;
 
 	public Long getId() {
 		return id;
@@ -95,6 +99,30 @@ public class Pessoa implements Serializable {
 
 	public void setAtivo(Boolean ativo) {
 		this.ativo = ativo;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public String getPerfilUser() {
+		return perfilUser;
+	}
+	
+	public void setPerfilUser(String perfilUser) {
+		this.perfilUser = perfilUser;
 	}
 
 	/*
