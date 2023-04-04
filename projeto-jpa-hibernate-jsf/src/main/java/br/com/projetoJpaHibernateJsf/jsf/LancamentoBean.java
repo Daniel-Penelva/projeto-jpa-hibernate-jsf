eac4ce87dc5e548d6366535dece093d5da20e483
Daniel Penelva <d4n.andrade@gmail.com>
2023-04-03 22:21:46 -0300

    Criando o managedBean de lançamentos

---
+++ projeto-jpa-hibernate-jsf/src/main/java/br/com/projetoJpaHibernateJsf/jsf/LancamentoBean.java
@@ -0,0 +1,70 @@
+package br.com.projetoJpaHibernateJsf.jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+import br.com.projetoJpaHibernateJsf.dao.DaoGeneric;
+import br.com.projetoJpaHibernateJsf.entidade.Lancamento;
+import br.com.projetoJpaHibernateJsf.entidade.Pessoa;
+
+@ViewScoped
+@ManagedBean(name = "lancamentoBean")
+public class LancamentoBean {
+
+	private Lancamento lancamento = new Lancamento();
+	private DaoGeneric<Lancamento> daoGeneric = new DaoGeneric<Lancamento>();
+	private List<Lancamento> lancamentos = new ArrayList<Lancamento>();
+	
+	
+	public String salvar() {
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = context.getExternalContext();
+		Pessoa pessoa = (Pessoa) externalContext.getSessionMap().get("usuarioLogado");
+		
+		lancamento.setUsuario(pessoa);
+		
+		daoGeneric.salvar(lancamento);
+		
+		return "";
+	}
+	
+	public String novo() {
+		
+		return "";
+	}
+	
+	public String remove() {
+		return "";
+	}
+
+	public Lancamento getLancamento() {
+		return lancamento;
+	}
+
+	public void setLancamento(Lancamento lancamento) {
+		this.lancamento = lancamento;
+	}
+
+	public DaoGeneric<Lancamento> getDaoGeneric() {
+		return daoGeneric;
+	}
+
+	public void setDaoGeneric(DaoGeneric<Lancamento> daoGeneric) {
+		this.daoGeneric = daoGeneric;
+	}
+
+	public List<Lancamento> getLancamentos() {
+		return lancamentos;
+	}
+
+	public void setLancamentos(List<Lancamento> lancamentos) {
+		this.lancamentos = lancamentos;
+	}
+
+}
